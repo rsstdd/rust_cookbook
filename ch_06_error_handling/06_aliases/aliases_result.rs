2e5a08d33cf5705cb5693956384befb0b99bf19c
Ross Todd <rosstod@gmail.com>
2019-02-06 19:57:14 -0800

    feat(aliases): Add example implementing aliases

---
+++ ch_06_error_handling/06_aliases/aliases_result.rs
@@ -0,0 +1,21 @@
+//! Implementing Aliases
+
+use std::num::ParseIntError;
+
+type AliasedResult<T> = Result<T, ParseIntError>;
+
+fn double_number(number_str: &str) -> AliasedResult<i32> {
+    number_str.parse::<i32>().map(|n| 2 * n)
+}
+
+fn print(result: AliasedResult<i32>) {
+    match result {
+        Ok(n) => println!("n is {}", n),
+        Err(e) => println!("Error: {}", e),
+    }
+}
+
+fn main() {
+    print(double_number("10"));
+    print(double_number("t"));
+}
