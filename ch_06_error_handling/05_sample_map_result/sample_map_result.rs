b822980f5b83b3f4a30fd0a41536131c2e1043fa
Ross Todd <rosstod@gmail.com>
2019-02-06 19:29:33 -0800

    feat(map_result): Add example of map_result

---
+++ ch_06_error_handling/05_sample_map_result/sample_map_result.rs
@@ -0,0 +1,28 @@
+//! sample_map_result
+use std::num::ParseIntError;
+
+fn double_number(number_str: &str) -> Result<i32, ParseIntError> {
+    match number_str.parse::<i32>() {
+        Ok(n) => Ok(2 * n),
+        Err(e) => Err(e),
+    }
+}
+
+fn double_number_map(number_str: &str) -> Result<i32, ParseIntError> {
+    number_str.parse::<i32>().map(|n| n * 2)
+}
+
+fn print_result(result: Result<i32, ParseIntError>) {
+    match result {
+        Ok(n) => println!("n is {}", n),
+        Err(e) => println!("Error: {}", e)
+    }
+}
+
+fn main() {
+    let twenty = double_number("10");
+    print_result(twenty);
+
+    let tt = double_number_map("t");
+    print_result(tt);
+}
