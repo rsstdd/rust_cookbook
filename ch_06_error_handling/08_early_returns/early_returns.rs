da68f17cd1ff3dba7be939531663e658959f0459
Ross Todd <rosstod@gmail.com>
2019-02-06 20:50:35 -0800

    feat(errors): Add example for early return

---
+++ ch_06_error_handling/08_early_returns/early_returns.rs
@@ -0,0 +1,32 @@
+//! Example of Early Returns
+
+type Result<T> = std::result::Result<T, String>;
+
+fn double_first(vec: Vec<&str>) -> Result<i32> {
+    let first = match vec.first() {
+        Some(first) => first,
+        None => return Err("Please use a vec with at least one element".to_owned())
+    };
+
+    match first.parse::<i32>() {
+        Ok(i) => Ok(2 * i),
+        Err(e) => Err(e.to_string())
+    }
+}
+
+fn print(result: Result<i32>) {
+    match result {
+        Ok(n) => println!("The first doubled is {}", n),
+        Err(e) => println!("The first error is {}", e),
+    }
+}
+
+fn main() {
+    let empty = vec![];
+    let strings = vec!["tofu", "93", "18"];
+    let nums = vec!["99", "93", "18"];
+
+    print(double_first(empty));
+    print(double_first(strings));
+    print(double_first(nums));
+}
