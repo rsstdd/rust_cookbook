ca1e4561bcfa144ec5a867d74170fddf1ee68be7
Ross Todd <rosstod@gmail.com>
2019-02-07 22:15:10 -0800

    feat(07 macros): Add common macros

---
+++ ch_07_macros/02_match/match.rs
@@ -0,0 +1,13 @@
+//! Implementing common macros in rust
+
+fn main() {
+    let v = vec![1, 2, 3, 4, 5];
+    print!("Vector :- {:?}", v);
+
+    assert!(true);
+    assert_eq!(5, 3 + 2);
+
+    // assert!(false);
+
+    // panic!("oh, no")
+}
